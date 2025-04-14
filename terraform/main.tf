7b69d468ae148541acb5bfee50252082690edf62
Jonathan Feldman <yoni101087@gmail.com>
2025-04-14 11:22:00 +0000

    added vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "vpc" {
 
   name                = "vpc"
   cidr                = "10.0.0.0/16"
-  availability_zones  = slice(data.aws_availability_zones.available.names, 0, 3)
+  availability_zones = ["us-west-2a"]
 
   private_subnets     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets      = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
